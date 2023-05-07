2c86e4998f9f01fd706e5003f865c1d69b58cb48
Parthakmehra <parthakmehra3@gmail.com>
2023-05-07 14:44:47 +0530

    App.js and App.css modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch('https://reqres.in/api/users/');
+      const { data } = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderUserCards = () => {
+    return users.map((user) => {
+      return (
+        <div key={user.id} className="user-card">
+          <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
+          <h2>{`${user.first_name} ${user.last_name}`}</h2>
+          <p>{user.email}</p>
+          <p>ID: {user.id}</p>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Cards</h1>
+      <button onClick={fetchUsers}>Get Users</button>
+      <div className="user-cards">{renderUserCards()}</div>
     </div>
   );
 }
