b42b8d6acf5f350679a13664b4e8c4af447db845
Parthakmehra <parthakmehra3@gmail.com>
2023-05-12 09:29:47 +0530

    App.module.css and App.js changed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-import React, { useState } from 'react';
-import './App.css';
+
+import styles from './App.module.css';
 
 function App() {
-  const [mobile, setMobile] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.card}>
+                    <img className='image' src='./mentor1.png' alt='mentor ' />
+                    <h4>Pritesh Kumar , IIT Delhi</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor2.png" alt='mentor'/>
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor2.png" alt="mentor " />
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor2.png" alt='mentor'/>
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor1.png" alt="mentor " />
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor1.png" alt="mentor " />
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor1.png" alt="mentor " />
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+                <div className={styles.card}>
+                    <img className='image' src="./mentor2.png" alt="mentor " />
+                    <h4>Anubhav singh , IIT Kanpur</h4>
+                    <p>CTO,Function Up</p>
+                    <p>9 yrs experience</p>
+                </div>
+            </div>
+        </div>
+    )
 
-  const handleMobileChange = (event) => {
-    setMobile(event.target.value);
-  };
-
-  const handleGenerateOTP = async () => {
-    try {
-      const response = await fetch('https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ mobile: parseInt(mobile) }),
-      });
-
-      if (!response.ok) {
-        const { error } = await response.json();
-        setErrorMessage(error);
-        return;
-      }
-
-      setErrorMessage('');
-      setMobile('');
-    } catch (error) {
-      console.error(error);
-      setErrorMessage('An error occurred while generating OTP.');
-    }
-  };
-
-  return (
-    <div className="App">
-      <h1>Generate OTP</h1>
-      <p>Enter your 10-digit mobile number:</p>
-      <input type="tel" value={mobile} onChange={handleMobileChange} pattern="[0-9]{10}" required />
-      <button onClick={handleGenerateOTP}>Generate OTP</button>
-      {errorMessage && <p className="error">{errorMessage}</p>}
-    </div>
-  );
 }
 
 export default App;
