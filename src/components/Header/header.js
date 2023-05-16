827b27deb6d3d7b6d753ed77dbe4a6552e62679b
Parthakmehra <parthakmehra3@gmail.com>
2023-05-16 22:52:04 +0530

    header.js changed

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,8 +1,22 @@
 import logo from '../../logo.svg';
 import styles from './header.module.css';
-// import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 export default function Header(){
+    const navigate = useNavigate();
+
+    function handleLogin(){
+        navigate('/login');
+    }
+
+    function handleSignUp(){
+        navigate('/signup');
+    }
+
+    function handleAbout(){
+        navigate('/about');
+    }
+
     return (
         <div className={styles.navbar}>
             <div className={styles.logo}>
@@ -11,11 +25,12 @@ export default function Header(){
             <div className={styles.list}>
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="#" onClick={handleAbout}>About</a></li>
                     <li><a href="#">Projects</a></li>
                     <li><a href="#">Videos</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><button className={styles.btn}>Login</button></li>
+                    <li><button className={styles.btn} onClick={handleLogin}>Login</button></li>
+                    <li><button className={styles.btn} onClick={handleSignUp}>SignUp</button></li>
                 </ul>
             </div>
         </div>
---
+++ src/components/pages/login.js
@@ -0,0 +1,5 @@
+export default function Login(){
+    return (
+        <p>I am a Login Page</p>
+    )
+}
